7f6e3f1248d2a0624795991f88c642e2b9adde38
Anton Blomberg <anton.blomberg@klarna.com>
2018-10-23 16:26:45 +0200

    Refactor swarm-setup

--- swarm/compose/haproxy/docker-entrypoint-override.sh
+++ swarm/compose/haproxy/docker-entrypoint-override.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-confgen.sh &
+/confgen.sh &
 /docker-entrypoint.sh $@
---
+++ swarm/include/common.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}"))")/.." || exit
+
+function get_nodes() {
+    cat nodelist.txt
+}
+
+function on_node() {
+    NODE="$1"
+    shift
+    (DOCKER_HOST=tcp://${NODE} docker $@)
+}
+
+function random_node() {
+    NODES=(`get_nodes`)
+    echo ${NODES[$RANDOM % ${#NODES[@]} ]}
+}
+
+function in_swarm() {
+    NODE=`random_node`
+    on_node ${NODE} ${@}
+}
